e75313cad14767508d81444044f25ac036da1748
Talha Ali <40734718@live.napier.ac.uk>
2025-03-06 20:09:24 +0000

    Add files via upload

---
+++ DitecLibrarySystem/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Name       : Talha Ali
+StudentID  : 40734718
+Module code: ENI08107
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DitecLibrarySystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
